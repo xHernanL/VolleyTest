9e35debe7e8448b8177042142d8e553cdb42ffd1
xHernanL <nano_manya07@hotmail.com>
2021-03-20 12:28:36 -0300

    Commit 20/03/21

--- app/src/main/java/com/example/volleypost/MainActivity.java
+++ app/src/main/java/com/example/volleypost/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.volleypost;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,24 +10,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.io.UnsupportedEncodingException;
-
-public class MainActivity extends AppCompatActivity implements Response.Listener<JsonObjectRequest>,Response.ErrorListener {
+public class MainActivity extends AppCompatActivity implements Response.Listener<JsonObjectRequest>, Response.ErrorListener {
 
     private EditText etId, etName, etSport;
     private Button btnSubmit;
+
+    ProgressDialog progreso;
 
     private RequestQueue request;
     JsonObjectRequest jsonObjectRequest;
@@ -50,12 +47,16 @@ public class MainActivity extends AppCompatActivity implements Response.Listener
 
             private void cargarWebService() {
 
+                progreso= new ProgressDialog(MainActivity.this);
+                progreso.setMessage("Cargando...");
+                progreso.show();
+
                 String url = "http://192.168.1.104:8081/BDRemota/wsJSONRegistro.php?id=" + etId.getText().toString() +
                         "&name=" + etName.getText().toString() +
                         "&sport=" + etSport.getText().toString();
 
 
-                jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,url,null,null,null);
+                jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,url,null,null,null );
                 request.add(jsonObjectRequest);
 
             }
@@ -65,13 +66,23 @@ public class MainActivity extends AppCompatActivity implements Response.Listener
     }
     @Override
     public void onResponse(JsonObjectRequest response) {
-        Toast.makeText(this,"Exito",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"Exito :)", Toast.LENGTH_SHORT).show();
+        progreso.hide();
+        etId.setText(" ");
+        etName.setText(" ");
+        etSport.setText(" ");
 
     }
     @Override
     public void onErrorResponse(VolleyError error) {
-        Toast.makeText(this,"Error",Toast.LENGTH_SHORT).show();
+        progreso.hide();
+        Toast.makeText(this,"Error :("+error.toString(), Toast.LENGTH_SHORT).show();
+        Log.i("Error",error.toString());
     }
 
 
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
 }
